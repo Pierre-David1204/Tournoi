78ed74945040721fbedbabc4d26c35edbe9d7947
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 14:27:22 +0100

    Add session state initialization for robot
    Initialize session state for robot if not present.

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from supabase import create_client
 url = "https://yzupjrzhqmojefurpmrx.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
 
+
 supabase = create_client(url, key)
 
+if "robot" not in st.session_state:
+    st.session_state.robot = None
+    
 st.title("Test Supabase")
 
 try:
@@ -13,3 +17,5 @@ try:
     st.write(data.data)
 except Exception as e:
     st.error(e)
+
+
