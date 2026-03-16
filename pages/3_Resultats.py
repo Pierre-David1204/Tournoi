eb33d86567cf318f622d3e314ac2cad83036a921
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 14:37:48 +0100

    Update Supabase API key in 3_Resultats.py
    Replaced placeholder API key with actual key for Supabase client.

--- pages/3_Resultats.py
+++ pages/3_Resultats.py
@@ -2,7 +2,7 @@ import streamlit as st
 from supabase import create_client
 
 url = "https://yzupjrzhqmojefurpmrx.supabase.co"
-key = "TA_CLE"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
 
 supabase = create_client(url, key)
 
