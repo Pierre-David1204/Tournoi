a6617f1d69aff8cc32c685ec7dc99336ab5899e9
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 15:27:16 +0100

    Update match display logic for final phase

--- pages/4_Phase_finale.py
+++ pages/4_Phase_finale.py
@@ -6,14 +6,28 @@ key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6
 
 supabase = create_client(url, key)
 
-st.title("🏆 Phase finale")
+if "equipe" not in st.session_state or st.session_state.equipe is None:
+    st.switch_page("app.py")
 
-matchs = supabase.table("phase_finale") \
+equipe = st.session_state.equipe
+id_equipe = equipe["id"]
+
+st.title("🤖 Mes matchs")
+
+matchs = supabase.table("matchs") \
     .select("*") \
     .execute()
 
 for m in matchs.data:
 
-    st.write(
-        f"{m['tour']} : {m['robot1']} vs {m['robot2']}"
-    )
+    if m["equipe1"] == id_equipe or m["equipe2"] == id_equipe:
+
+        statut = "⏳ à jouer"
+
+        if m["termine"]:
+            statut = "✅ terminé"
+
+        st.write(
+            f"Terrain {m['terrain']} | {m['heure']} | "
+            f"{m['equipe1']} vs {m['equipe2']} | {statut}"
+        )
