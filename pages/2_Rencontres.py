fe42c8286ea35410c2547c1d46abe53a80753268
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 14:32:48 +0100

    Update 2_Rencontres.py

--- pages/2_Rencontres.py
+++ pages/2_Rencontres.py
@@ -1,22 +1,27 @@
 import streamlit as st
-from utils.supabase_client import supabase
+from supabase import create_client
 
-st.title("⚔️ Rencontres")
+url = "https://yzupjrzhqmojefurpmrx.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
+
+supabase = create_client(url, key)
+
+if "robot" not in st.session_state or st.session_state.robot is None:
+    st.warning("Veuillez vous connecter")
+    st.switch_page("app.py")
 
 robot = st.session_state.robot
 poule_id = robot["poule_id"]
 
+st.title("⚔️ Rencontres")
+
 matchs = supabase.table("matchs") \
-    .select("*, robots!robot1(nom), robots!robot2(nom)") \
+    .select("*") \
     .eq("poule_id", poule_id) \
     .execute()
 
 for m in matchs.data:
 
-    r1 = m["robots"]["nom"]
-    r2 = m["robots_1"]["nom"]
-
-    if m["termine"]:
-        st.write(f"✅ {r1} vs {r2}")
-    else:
-        st.write(f"⏳ {r1} vs {r2}")
+    st.write(
+        f"{m['robot1']} vs {m['robot2']} | terminé : {m['termine']}"
+    )
