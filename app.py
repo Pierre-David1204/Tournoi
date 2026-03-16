bb0f0e01b0b968c8fe9c7d89ec406af89b08aae6
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 14:31:34 +0100

    Implement robot login feature with Supabase
    Add robot connection functionality with Supabase.

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from supabase import create_client
 url = "https://yzupjrzhqmojefurpmrx.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
 
-
 supabase = create_client(url, key)
+
+st.title("🤖 Tournoi Robot")
 
 if "robot" not in st.session_state:
     st.session_state.robot = None
-    
-st.title("Test Supabase")
 
-try:
-    data = supabase.table("robots").select("*").execute()
-    st.write(data.data)
-except Exception as e:
-    st.error(e)
+robot_name = st.text_input("Nom du robot")
 
+if st.button("Connexion"):
+
+    res = supabase.table("robots") \
+        .select("*") \
+        .eq("nom", robot_name) \
+        .execute()
+
+    if res.data:
+        st.session_state.robot = res.data[0]
+        st.success("Connexion réussie")
+        st.switch_page("pages/1_Classement.py")
+
+    else:
+        st.error("Robot inconnu")
 
