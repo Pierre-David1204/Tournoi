c7db11eef6cfe90eb3ef12ef75f21d48b7fce00c
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-16 15:24:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,29 @@
 import streamlit as st
 from supabase import create_client
 
-# connexion supabase
 url = "https://yzupjrzhqmojefurpmrx.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
 
 supabase = create_client(url, key)
 
-st.title("🤖 Tournoi Robot - Connexion")
+st.title("🤖 Tournoi D2")
 
-# session
 if "equipe" not in st.session_state:
     st.session_state.equipe = None
 
-
-# récupérer les équipes
 data = supabase.table("equipes").select("*").order("nom").execute()
-
 equipes = data.data
 
-noms_equipes = [e["nom"] for e in equipes]
+noms = [e["nom"] for e in equipes]
 
-
-# menu déroulant
-equipe_selection = st.selectbox(
-    "Choisissez votre équipe",
-    noms_equipes
-)
-
+selection = st.selectbox("Choisissez votre équipe", noms)
 
 if st.button("Se connecter"):
 
-    equipe = next(e for e in equipes if e["nom"] == equipe_selection)
+    equipe = next(e for e in equipes if e["nom"] == selection)
 
     st.session_state.equipe = equipe
 
-    st.success(f"Connecté : {equipe_selection}")
+    st.success("Connexion réussie")
 
     st.switch_page("pages/1_Classement.py")
