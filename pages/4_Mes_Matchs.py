5b6d983309a0a1a2a47cf60d6b839a0f79abd418
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-17 09:17:50 +0100

    Refactor match retrieval and display logic

--- pages/4_Mes_Matchs.py
+++ pages/4_Mes_Matchs.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from supabase import create_client
 import pandas as pd
+from supabase import create_client
 
 url = "https://yzupjrzhqmojefurpmrx.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
@@ -12,33 +12,41 @@ if "equipe" not in st.session_state or st.session_state.equipe is None:
 
 equipe = st.session_state.equipe
 id_equipe = equipe["id"]
+mon_equipe = equipe["nom"]
 
 st.title("🤖 Mes matchs")
-
-matchs = supabase.table("matchs") \
-    .select("*") \
-    .execute()
 
 # récupérer équipes
 equipes_data = supabase.table("equipes").select("*").execute()
 equipes = {e["id"]: e["nom"] for e in equipes_data.data}
-heure_affichee = pd.to_datetime(str(m["heure"])).strftime("%H:%M")
 
-for m in matchs.data:
+# récupérer matchs
+matchs = supabase.table("matchs") \
+    .select("*") \
+    .order("heure") \
+    .execute()
 
-    if m["equipe1"] == id_equipe or m["equipe2"] == id_equipe:
+df = pd.DataFrame(matchs.data)
 
-        equipe1 = equipes.get(m["equipe1"], m["equipe1"])
-        equipe2 = equipes.get(m["equipe2"], m["equipe2"])
+df["Equipe1"] = df["equipe1"].map(equipes)
+df["Equipe2"] = df["equipe2"].map(equipes)
 
-        statut = "⏳ à jouer"
+# garder seulement mes matchs
+df = df[(df["equipe1"] == id_equipe) | (df["equipe2"] == id_equipe)]
 
-        if m["termine"]:
-            statut = "✅ terminé"
+for _, m in df.iterrows():
 
-        st.write(
-            f"Terrain {m['terrain']} | {heure_addichee} | "
-            f"{equipe1} vs {equipe2} | {statut}"
-        )
+    heure_affichee = pd.to_datetime(str(m["heure"])).strftime("%H:%M")
+
+    statut = "⏳"
+
+    if m["termine"]:
+        statut = "✅"
+
+    texte = f"{m['Equipe1']} vs {m['Equipe2']}"
+
+    st.write(
+        f"{heure_affichee} | Terrain {m['terrain']} | {texte} {statut}"
+    )
 
 
